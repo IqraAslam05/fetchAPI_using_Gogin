c36b49f134f13c26b547512b8018aae3063078ad
IqraAslam05@ <iqra37232@gmail.com>
2023-08-26 16:28:37 +0500

    code check

--- gin.go
+++ gin.go
@@ -1,7 +1,10 @@
 package main
- import
- "fmt"
- func main(){
- 	var n=[]int{2,3,4,56,7} //declaring and initializing array
+
+import "fmt"
+
+func main() {
+	var n = []int{2, 3, 4, 56, 7} //declaring and initializing array
 	fmt.Println(n)
- }
+	var a = []int{2, 3, 4, 56, 7} //declaring and initializing array
+	fmt.Println(a)
+}
